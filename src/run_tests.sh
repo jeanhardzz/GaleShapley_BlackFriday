6ee23faeac77e74b23639faa7f90c802e782eba5
Fulano de Tal <jean.lk@hotmail.com>
2021-11-22 15:54:12 -0300

    make test

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -1,8 +1,8 @@
 EXEC=$1
 TMP_OUT=$2
 
-for i in {0..9..1}; do
-  testname=$(printf "%01d" $i)
+for i in {1..4..1}; do
+  testname=$(printf caso_teste_0"%01d" $i)
   $EXEC < tests/$testname.in > $TMP_OUT
   if ! diff -qwB tests/$testname.out $TMP_OUT &>/dev/null; then
     echo "Test $testname failed"
